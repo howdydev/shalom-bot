ff7aaa1945133c7f10a59113c1915eb04fa4755e
howdydev <163334316+howdydev@users.noreply.github.com>
2024-04-09 16:48:46 +0100

    givegp command

--- src/commands/Economy/givegp.ts
+++ src/commands/Economy/givegp.ts
@@ -1,11 +1,11 @@
-import { PermissionsBitField, SlashCommandBuilder, User } from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
 import { CommandType } from "../../types/command";
 import { formatNumber } from "../../utils";
 
 export const command: CommandType = {
   builder: new SlashCommandBuilder()
-    .setName("admingivegp")
-    .setDescription("Give a user some GP (Admin only)")
+    .setName("givegp")
+    .setDescription("Give a user some GP")
     .addUserOption((option) =>
       option
         .setName("user")
@@ -17,32 +17,56 @@ export const command: CommandType = {
         .setName("amount")
         .setDescription("The amount of GP you wish to give to the user")
         .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
+    ),
   run: async (client, interaction) => {
     await interaction.deferReply({
       ephemeral: true,
     });
 
     try {
-      const target = interaction.options.getUser("user", true) as User;
+      const target = interaction.options.getUser("user", true);
       const amount = interaction.options.getInteger("amount", true);
-      const success = await client.members.addGP(target.id, amount);
 
-      if (!success)
+      const member = client.members.getMemberData(target.id);
+      if (!member)
         return await interaction.editReply(
           `I could not give <:gp:1171046821137760309> ${formatNumber(
             amount
           )} to <@${target.id}>. Maybe they've not registered?`
         );
 
-      return await interaction.editReply(
+      const success = await client.members.removeGP(
+        interaction.user.id,
+        amount
+      );
+
+      if (!success)
+        return await interaction.editReply(
+          `I could not give <:gp:1171046821137760309> ${formatNumber(
+            amount
+          )} to <@${target.id}>. You do not have enough GP for this.`
+        );
+
+      await client.members.addGP(target.id, amount);
+
+      await interaction.editReply(
         `I have successfully given <@${
           target.id
         }> <:gp:1171046821137760309> ${formatNumber(
           amount
         )}. Their balance is now <:gp:1171046821137760309> ${formatNumber(
           +success
+        )}`
+      );
+
+      //   dm user with gp given
+      await target.send(
+        `You have been given <:gp:1171046821137760309> ${formatNumber(
+          amount
+        )} by <@${
+          interaction.user.id
+        }>. Your balance is now <:gp:1171046821137760309> ${formatNumber(
+          member.gp + amount
         )}`
       );
     } catch (error) {
