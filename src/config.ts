9337509df70dddbf2d39237c69b640b0240c94d2
howdydev <163334316+howdydev@users.noreply.github.com>
2024-04-10 10:13:18 +0100

    remove general settings from env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   guildId: process.env.GUILD_ID as string,
-  triviaEnabled: process.env.TRIVIA_ENABLED === "true",
-  kickWatchEnabled: process.env.KICK_WATCH_ENABLED === "true",
+  triviaEnabled: true,
+  kickWatchEnabled: false,
   channels: {
     TRIVIA: process.env.TRIVIA_CHANNEL_ID as string,
     SLAYER_TASKS: process.env.SLAYER_TASKS_CHANNEL_ID as string,
