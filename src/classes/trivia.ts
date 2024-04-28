6c9e1d3607befeb92fc910edc86ce93793a212d8
howdydev <163334316+howdydev@users.noreply.github.com>
2024-04-28 06:54:38 +0100

    update trivia

--- src/classes/trivia.ts
+++ src/classes/trivia.ts
@@ -1,133 +1,143 @@
-import triviaQuestions from "../utils/trivia-questions";
 import ExtendedClient from "./extended-client";
 import config from "../config";
 import { EmbedBuilder, TextChannel } from "discord.js";
 import { TriviaQuestion } from "@prisma/client";
 
 export default class TriviaGenerator {
-  private readonly client: ExtendedClient;
-  public readonly questions: TriviaQuestion[] = [];
-  public currentQuestion: TriviaQuestion | null = null;
-  public currentAnswers: string[] = [];
-  public reward: number;
-  public isReady: boolean = false;
+	private readonly client: ExtendedClient;
+	public readonly questions: TriviaQuestion[] = [];
+	public currentQuestion: TriviaQuestion | null = null;
+	public currentAnswers: string[] = [];
+	public reward: number;
+	public isReady: boolean = false;
+	public guesses: [string, string][] = [];
 
-  constructor(client: ExtendedClient) {
-    this.client = client;
-  }
+	constructor(client: ExtendedClient) {
+		this.client = client;
+	}
 
-  public async init() {
-    console.info("Trivia: Initializing");
-    await this.fetchAllQuestions();
-    console.success(`Trivia: Loaded ${this.questions.length} questions`);
-    this.shuffleQuestions();
-    this.startGeneratingQuestions();
-    this.isReady = true;
-  }
+	public async init() {
+		await this.fetchAllQuestions();
+		this.shuffleQuestions();
+		this.startGeneratingQuestions();
+		this.isReady = true;
+	}
 
-  private async fetchAllQuestions() {
-    const result = await this.client.database.triviaQuestion.findMany();
-    this.questions.push(...result);
-  }
+	private async fetchAllQuestions() {
+		const result = await this.client.database.triviaQuestion.findMany();
+		this.questions.push(...result);
+	}
 
-  public isTriviaActive() {
-    return this.currentQuestion !== null;
-  }
+	public isTriviaActive() {
+		return this.currentQuestion !== null;
+	}
 
-  public getTriviaAnswers() {
-    return this.currentAnswers;
-  }
+	public addGuess(guess: string, user: string) {
+		const exists = this.guesses.find(([g]) => g === guess);
+		if (exists) return;
+		this.guesses.push([guess, user]);
+	}
 
-  public endTrivia() {
-    this.currentQuestion = null;
-  }
+	public fetchGuesses() {
+		return this.guesses;
+	}
 
-  public async startGeneratingQuestions() {
-    if (this.questions.length === 0) {
-      console.error(`Trivia: No questions found`);
-      return;
-    }
+	public getTriviaAnswers() {
+		return this.currentAnswers;
+	}
 
-    this.generateQuestion();
-    setInterval(this.generateQuestion.bind(this), 25 * 60 * 1000);
-    // setInterval(this.generateQuestion.bind(this), 30000);
-  }
+	public endTrivia() {
+		this.currentQuestion = null;
+		this.guesses = [];
+	}
 
-  public async generateQuestion() {
-    await this.clearTriviaChannel();
-    const selectedQuestion = this.questions.shift() as TriviaQuestion;
-    this.questions.push(selectedQuestion);
-    this.currentQuestion = selectedQuestion;
-    this.reward = Math.floor(Math.random() * 100000) + 10000;
-    this.currentAnswers = selectedQuestion.answer.split(", ");
-    await this.sendTriviaQuestionToChannel(
-      selectedQuestion.question,
-      selectedQuestion.id
-    );
-  }
+	public async startGeneratingQuestions() {
+		if (this.questions.length === 0) {
+			console.error(`Trivia: No questions found`);
+			return;
+		}
 
-  public async clearTriviaChannel() {
-    const guild = await this.client.guilds.fetch(config.guildId);
-    if (!guild) return console.error("Trivia: Guild not found");
+		this.generateQuestion();
+		setInterval(this.generateQuestion.bind(this), 25 * 60 * 1000);
+	}
 
-    const channel = await guild.channels.fetch(config.channels.TRIVIA);
-    if (!channel || !(channel instanceof TextChannel))
-      return console.error("Trivia: Channel not found");
+	public async generateQuestion() {
+		await this.clearTriviaChannel();
+		const selectedQuestion = this.questions.shift() as TriviaQuestion;
 
-    for (let i = 0; i < 10; i++)
-      await (channel as TextChannel).bulkDelete(100).catch(console.error);
-  }
+		this.questions.push(selectedQuestion);
+		this.currentQuestion = selectedQuestion;
+		this.reward = Math.floor(Math.random() * 100000) + 10000;
+		this.currentAnswers = selectedQuestion.answer.split(", ");
 
-  public async addTriviaQuestion(data: {
-    question: string;
-    answer: string;
-    createdBy: string;
-  }) {
-    const success = await this.client.database.triviaQuestion.create({
-      data,
-    });
+		await this.sendTriviaQuestionToChannel(
+			selectedQuestion.question,
+			selectedQuestion.id
+		);
+	}
 
-    if (!success) return false;
+	public async clearTriviaChannel() {
+		const guild = await this.client.guilds.fetch(config.guildId);
+		if (!guild) return console.error("Trivia: Guild not found");
 
-    this.questions.push(success);
-    return success.id;
-  }
+		const channel = await guild.channels.fetch(config.channels.TRIVIA);
+		if (!channel || !(channel instanceof TextChannel))
+			return console.error("Trivia: Channel not found");
 
-  public async sendTriviaQuestionToChannel(
-    question: string,
-    questionId: string
-  ) {
-    const guild = await this.client.guilds.fetch(config.guildId);
-    const embed = new EmbedBuilder()
-      .setTitle("Trivia")
-      .setDescription(
-        `A new trivia is active, you have 25 minutes to answer the question correctly.`
-      )
-      .addFields({ name: "Question", value: question })
-      .setThumbnail(
-        "https://oldschool.runescape.wiki/images/thumb/Wise_Old_Man.png/130px-Wise_Old_Man.png?b2e69"
-      )
-      .setFooter({
-        text: `Question ID: ${questionId}`,
-      });
-    if (!guild) return console.error("Trivia: Guild not found");
+		for (let i = 0; i < 10; i++)
+			await (channel as TextChannel).bulkDelete(100).catch(console.error);
+	}
 
-    const channel = await guild.channels.fetch(config.channels.TRIVIA);
-    if (!channel || !(channel instanceof TextChannel))
-      return console.error("Trivia: Channel not found");
+	public async addTriviaQuestion(data: {
+		question: string;
+		answer: string;
+		createdBy: string;
+	}) {
+		const success = await this.client.database.triviaQuestion.create({
+			data,
+		});
 
-    await (channel as TextChannel).send({
-      embeds: [embed],
-    });
-  }
+		if (!success) return false;
 
-  private shuffleQuestions() {
-    for (let i = this.questions.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.questions[i], this.questions[j]] = [
-        this.questions[j],
-        this.questions[i],
-      ];
-    }
-  }
+		this.questions.push(success);
+		return success.id;
+	}
+
+	public async sendTriviaQuestionToChannel(
+		question: string,
+		questionId: string
+	) {
+		const guild = await this.client.guilds.fetch(config.guildId);
+		const embed = new EmbedBuilder()
+			.setTitle("Trivia")
+			.setDescription(
+				`A new trivia is active, you have 25 minutes to answer the question correctly.`
+			)
+			.addFields({ name: "Question", value: question })
+			.setThumbnail(
+				"https://oldschool.runescape.wiki/images/thumb/Wise_Old_Man.png/130px-Wise_Old_Man.png?b2e69"
+			)
+			.setFooter({
+				text: `Question ID: ${questionId}`,
+			});
+		if (!guild) return console.error("Trivia: Guild not found");
+
+		const channel = await guild.channels.fetch(config.channels.TRIVIA);
+		if (!channel || !(channel instanceof TextChannel))
+			return console.error("Trivia: Channel not found");
+
+		await (channel as TextChannel).send({
+			embeds: [embed],
+		});
+	}
+
+	private shuffleQuestions() {
+		for (let i = this.questions.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this.questions[i], this.questions[j]] = [
+				this.questions[j],
+				this.questions[i],
+			];
+		}
+	}
 }
--- src/events/messageCreateEvent.ts
+++ src/events/messageCreateEvent.ts
@@ -1,66 +1,91 @@
-import { Events, Message } from "discord.js";
+import { EmbedBuilder, Events, Message } from "discord.js";
 import ExtendedClient from "../classes/extended-client";
 import { Event as EventType } from "../types/event";
 import config from "../config";
 
 export const event: EventType = {
-  name: Events.MessageCreate,
-  type: "on",
-  run: async (client: ExtendedClient, message: Message) => {
-    if (message.author.bot) return;
+	name: Events.MessageCreate,
+	type: "on",
+	run: async (client: ExtendedClient, message: Message) => {
+		if (message.author.bot) return;
 
-    try {
-      const userId = message.author.id;
-      const channelID = message.channelId;
-      const channel = message.channel;
+		try {
+			const userId = message.author.id;
+			const channelID = message.channelId;
+			const channel = message.channel;
 
-      if (channelID === config.channels.TRIVIA) {
-        const answer = message.content;
+			if (channelID === config.channels.TRIVIA) {
+				const answer = message.content;
 
-        const member = client.members.getMemberData(userId);
-        if (!member) return;
+				const member = client.members.getMemberData(userId);
+				if (!member) return;
 
-        if (!client.trivia.isTriviaActive()) {
-          const botMsg = await message.reply({
-            content: "There is no trivia active at the moment.",
-          });
+				if (!client.trivia.isTriviaActive()) {
+					const botMsg = await message.reply({
+						content: "There is no trivia active at the moment.",
+					});
 
-          setTimeout(async () => {
-            await message.delete();
-            await botMsg.delete();
-          }, 5000);
+					setTimeout(async () => {
+						await message.delete();
+						await botMsg.delete();
+					}, 5000);
 
-          return;
-        }
+					return;
+				}
 
-        const answers = client.trivia.getTriviaAnswers();
-        console.log(answers);
-        if (!answers) {
-          message.reply({
-            content:
-              "An unknown error occurred, please wait for the next trivia.",
-          });
+				const answers = client.trivia.getTriviaAnswers();
 
-          return client.trivia.endTrivia();
-        }
+				if (!answers) {
+					message.reply({
+						content:
+							"An unknown error occurred, please wait for the next trivia.",
+					});
 
-        await message.delete();
+					return client.trivia.endTrivia();
+				}
 
-        const answersToLowercase = answers.map((answer) =>
-          answer.toLowerCase()
-        );
-        if (!answersToLowercase.includes(answer.toLowerCase())) return;
+				await message.delete();
 
-        if (!client.trivia.isTriviaActive()) return;
-        channel.send({
-          content: `**<@${userId}> has won**. The answer was \`${answers[0]}\`, you have been awarded <:gp:1171046821137760309> ${client.trivia.reward}.`,
-        });
+				const answersToLowercase = answers.map((answer) =>
+					answer.toLowerCase()
+				);
 
-        client.trivia.endTrivia();
-        client.members.addGP(userId, client.trivia.reward);
-      }
-    } catch (error) {
-      console.error(`messageCreateEvent.ts: ${error}`);
-    }
-  },
+				if (!answersToLowercase.includes(answer.toLowerCase())) {
+					client.trivia.addGuess(answer, userId);
+					return;
+				}
+
+				if (!client.trivia.isTriviaActive()) return;
+
+				const guesses = client.trivia.fetchGuesses();
+				const embed =
+					guesses.length > 0
+						? new EmbedBuilder()
+								.setColor("Random")
+								.setTitle(`Guess History`)
+								.setDescription(
+									`Here are all of the user guesses in the current trivia (Not including the correct answer)`
+								)
+								.addFields(
+									guesses.map(([guess, user]) => ({
+										name: `<@${user}>`,
+										value: `${guess}`,
+										inline: true,
+									}))
+								)
+						: null;
+
+				await channel.send({
+					content: `**<@${userId}> has won**, you have been awarded <:gp:1171046821137760309> ${client.trivia.reward}.`,
+				});
+
+				if (embed) await channel.send({ embeds: [embed] });
+
+				client.trivia.endTrivia();
+				client.members.addGP(userId, client.trivia.reward);
+			}
+		} catch (error) {
+			console.error(`messageCreateEvent.ts: ${error}`);
+		}
+	},
 };
